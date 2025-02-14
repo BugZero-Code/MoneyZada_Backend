95a955a3548be17c5a022a2489b54ec438da7165
IuryAlmeidaDev <iury.colombo@gmail.com>
2025-02-14 09:51:03 -0300

    Teste unitario de UserController OK

---
+++ src/test/java/br/com/iuryalmeida/MoneyZada/controller/UserControllerTest.java
@@ -0,0 +1,61 @@
+package br.com.iuryalmeida.MoneyZada.controller;
+
+import br.com.iuryalmeida.MoneyZada.repository.UserRepository;
+import br.com.iuryalmeida.MoneyZada.user.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @SuppressWarnings("removal")
+    @MockBean
+    private UserRepository userRepository;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        user.setName("Iury Almeida");
+        user.setEmail("iury.colombo@gmail.com");
+        user.setPassword("securePassword");
+    }
+
+    @Test
+    void registerUser_ShouldReturn201_WhenValidUser() throws Exception {
+        Mockito.when(userRepository.save(Mockito.any(User.class))).thenReturn(user);
+
+        mockMvc.perform(post("/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isCreated())
+                .andExpect(content().string("User registered successfully"));
+    }
+
+    @Test
+    void registerUser_ShouldReturn400_WhenInvalidUser() throws Exception {
+        
+        User invalidUser = new User();
+
+        mockMvc.perform(post("/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(invalidUser)))
+                .andExpect(status().isBadRequest());
+    }
+}
